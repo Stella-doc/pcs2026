47f5c7b6dd779563cc947672789da0a5bf434e0e
Stella <dimaggios81@gmail.com>
2026-03-18 16:42:47 +0000

    consegna_es2

---
+++ ESERCITAZIONE_2/build-esercitazione2/CMakeFiles/secondo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secondo.dir/link.d"
+  "CMakeFiles/secondo.dir/esercizio2.cpp.o"
+  "CMakeFiles/secondo.dir/esercizio2.cpp.o.d"
+  "secondo"
+  "secondo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secondo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
